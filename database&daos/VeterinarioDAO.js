dcfcf482c6c3a8d298aa6bb7d93596e44c1f6dc4
Vpazinatto <40928534+Vpazinatto@users.noreply.github.com>
2018-10-28 16:07:24 -0300

    src

--- database&daos/VeterinarioDAO.js
+++ database&daos/VeterinarioDAO.js
@@ -2,6 +2,14 @@ function veterinarioDAO(connection) {
     this._connection = connection;
 }
 
-veterinarioDAO.prototype.lista = function() {
+veterinarioDAO.prototype.lista = function(callback) {
+    this._connection.query('SELECT * FROM veterinario', callback);
+}
+
+veterinarioDAO.prototype.salva = function(veterinario, callback) {
+    this._connection.query("INSERT INTO veterinario VALUES ('" + veterinario.nome + "', '" +  veterinario.CPF + "')", callback);
+}
+
+veterinarioDAO.prototype.buscaPorID = function(id, callback) {
     
 }
